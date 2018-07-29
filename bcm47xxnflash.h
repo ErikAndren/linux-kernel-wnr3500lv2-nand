a17ab02872eca583cb10a5f7bcd4babb9631b16e
Erik Andrén Zachrisson <erik@zachrisson.info>
2018-07-29 19:23:37 +0200

    Initial add of 5357 nand driver stub

--- bcm47xxnflash.h
+++ bcm47xxnflash.h
@@ -22,5 +22,6 @@ struct bcm47xxnflash {
 };
 
 int bcm47xxnflash_ops_bcm4706_init(struct bcm47xxnflash *b47n);
+int bcm47xxnflash_ops_bcm5357_init(struct bcm47xxnflash *b47n);
 
 #endif /* BCM47XXNFLASH */
